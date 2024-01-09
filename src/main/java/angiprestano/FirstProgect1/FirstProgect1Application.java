9eb96f31ef95201785e27c79a07a21860677c77f
Angela Prestano <angiprestano99@gmail.com>
2024-01-09 17:03:39 +0100

    progress

--- src/main/java/angiprestano/FirstProgect1/FirstProgect1Application.java
+++ src/main/java/angiprestano/FirstProgect1/FirstProgect1Application.java
@@ -1,13 +1,18 @@
 package angiprestano.FirstProgect1;
 
+import angiprestano.FirstProgect1.entities.PizzaMargherita;
+import angiprestano.FirstProgect1.entities.PizzaSalame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class FirstProgect1Application {
 
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(FirstProgect1Application.class);
 		SpringApplication.run(FirstProgect1Application.class, args);
+		System.out.println("PizzaList");
+		System.out.println(ctx.getBean("Margherita"));
 	}
-
 }
