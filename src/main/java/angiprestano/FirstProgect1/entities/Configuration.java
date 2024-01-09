2132d8fc33488203c21f821b35a8c80de3cf61a4
Angela Prestano <angiprestano99@gmail.com>
2024-01-09 16:33:19 +0100

    pizze completate

--- src/main/java/angiprestano/FirstProgect1/entities/Configuration.java
+++ src/main/java/angiprestano/FirstProgect1/entities/Configuration.java
@@ -9,10 +9,33 @@ public class Configuration {
     public PizzaMargherita Margherita() {
         PizzaMargherita pizzaMargherita = new PizzaMargherita();
         pizzaMargherita.setIngredient1("Tomato");
-        pizzaMargherita.setIngredient2("cheese");
+        pizzaMargherita.setIngredient2("Cheese");
         pizzaMargherita.setCalories(1010);
         pizzaMargherita.setPrice(4.99);
         return pizzaMargherita;
     }
-    
+
+    @Bean
+    public PizzaHawain Hawain() {
+        PizzaHawain pizzaHawain = new PizzaHawain();
+        pizzaHawain.setIngredient1("Tomato");
+        pizzaHawain.setIngredient2("Cheese");
+        pizzaHawain.setHam("Ham");
+        pizzaHawain.setPineapple("Pineapple");
+        pizzaHawain.setCalories(1024);
+        pizzaHawain.setPrice(6.99);
+        return pizzaHawain;
+    }
+
+    @Bean
+    public PizzaSalame Salame() {
+        PizzaSalame pizzaSalame = new PizzaSalame();
+        pizzaSalame.setIngredient1("Tomato");
+        pizzaSalame.setIngredient2("Cheese");
+        pizzaSalame.setSalame("Salami");
+        pizzaSalame.setCalories(1160);
+        pizzaSalame.setPrice(5.99);
+        return pizzaSalame;
+    }
+
 }
